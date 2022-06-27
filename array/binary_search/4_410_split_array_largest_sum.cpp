7cd6a2136f3bf889159745990cf128858de7bf93
Chen Biao <chenbiao1@momenta.ai>
2022-06-27 08:46:29 +0800

    finish 410

--- array/binary_search/4_410_split_array_largest_sum.cpp
+++ array/binary_search/4_410_split_array_largest_sum.cpp
@@ -7,6 +7,9 @@ using namespace std;
 class Solution {
 public:
     int splitArray(vector<int>& nums, int m) {
+        if (nums.size() == 1){  // 在leecode上不加这一条会越界
+            return nums[0];
+        }
         long l = max(nums);
         long r = sum(nums);
         while (l < r){
@@ -17,18 +20,18 @@ public:
                 l = mid + 1;
             }
         }
-        return r;
+         return r;
     }
     bool canSplit(const vector<int> &nums, long max_sum, long need_split_nums){
         int splited_nums = 0;
         int sum = 0;
         for (int i = 0; i < nums.size(); i++){
-            if (splited_nums >= need_split_nums){
-                return false;
-            }
             if (sum + nums[i] > max_sum){
-                sum = 0;
+                sum = nums[i];
                 splited_nums ++;
+                if (splited_nums >= need_split_nums){
+                    return false;
+                }
             } else {
                 sum += nums[i];
             }
@@ -45,13 +48,13 @@ public:
         return min_val;
     }
     int max(const vector<int>&nums){
-        int min_val = nums[1];
+        int max_val = nums[1];
         for (int i = 1; i < nums.size(); i++){
-            if (min_val < nums[i]){
-                min_val = nums[i];
+            if (max_val < nums[i]){
+                max_val = nums[i];
             }
         }
-        return min_val;
+        return max_val;
     }
     long sum(const vector<int>&nums){
         int sum = 0;
@@ -64,8 +67,10 @@ public:
 
 
 int main(){
-    vector<int> nums = {10, 2, 3, 5, 8, 6, 4};
+    // vector<int> nums = {10, 2, 3, 5, 8, 6, 4};
+    vector<int> nums = {0};
     Solution solution;
-    solution.splitArray(nums, 3);
+    // solution.canSplit(nums, 14, 3);
+    solution.splitArray(nums, 1);
     return 0;
 }
