3b5d22714adefda119dc44df7e1aa4b2c3c44f3f
Chen Biao <chenbiao1@momenta.ai>
2022-07-07 23:57:01 +0800

    tree, finish 129

---
+++ tree/129_sum_root_to_leaf_numbers.cpp
@@ -0,0 +1,31 @@
+#include "tree_node.hpp"
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return sumNumbers(root, 0);
+    }
+
+    int sumNumbers(TreeNode *node, int sum){
+        if(!node)
+            return 0;
+        if (!node->left && !node->right){
+            return sum*10+node->val;
+        }
+        sum = sum * 10 + node->val;
+        int left_sum = sumNumbers(node->left, sum);
+        int right_sum = sumNumbers(node->right, sum);
+        return left_sum + right_sum;
+    }
+};
+
+
+
+int main(){
+    // vector<int> array = {-10, 9, 20, INT32_MIN, INT32_MIN,15,7};
+    vector<int> array = {1,2,3};
+    TreeNode* root = TreeNode::constructTree(array);
+    Solution s;
+    int sum = s.sumNumbers(root);
+    std::cout<< "Sum:" << sum << std::endl;
+}
--- tree/tree_node.hpp
+++ tree/tree_node.hpp
@@ -2,14 +2,54 @@
 
 #include <vector>
 #include <queue>
+#include <limits.h>
+#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#include <iostream>
 using namespace std;
 
+
+
+
 // Definition for a binary tree node.
-struct TreeNode {
+class TreeNode {
+public:
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    static TreeNode* constructTree(vector<int> array){
+        TreeNode *root;
+        queue<TreeNode*> q;
+        if (array.size() == 0){
+            root = nullptr;
+        }
+        int idx = 1;
+        root = new TreeNode(array[0]);
+        q.push(root);
+        while (idx < array.size()){
+            TreeNode *temp_node = q.front();
+            q.pop();
+            if (temp_node){
+                temp_node->left = TreeNode::constructTreeNode(array[idx++]);
+                q.push(temp_node->left);
+                if (idx == array.size())
+                    break;
+                temp_node->right = TreeNode::constructTreeNode(array[idx++]);
+                q.push(temp_node->right);
+            } else {
+                idx += 2;
+            }
+        }
+        return root;
+    }
+private:
+    static TreeNode *constructTreeNode(int val){
+        if (val == INT32_MIN){
+            return nullptr;
+        }
+        return new TreeNode(val);
+    }
 };
